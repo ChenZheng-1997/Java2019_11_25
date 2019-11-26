557bd7804c461aafb5313123ffbdf09de5c5a0da
ChenZheng-1997 <837846584@qq.com>
2019-11-26 10:48:11 +0800

    11

---
+++ ssm2019_11_21pm/src/main/java/com/domain/City.java
@@ -0,0 +1,64 @@
+package com.domain;
+
+public class City {
+    public City() {
+    }
+
+    public City(Integer id, String name, String countrycode, String district, Integer population) {
+        this.id = id;
+        this.name = name;
+        this.countrycode = countrycode;
+        this.district = district;
+        this.population = population;
+    }
+
+    private Integer id;
+
+    private String name;
+
+    private String countrycode;
+
+    private String district;
+
+    private Integer population;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
